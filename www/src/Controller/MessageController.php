cac7b0e817d1e37e45e35aaea090d7a9fe742a7f
Massimo Biagioli <massimo.biagioli@ideato.info>
2020-05-22 10:18:51 +0200

    Risolti conflitti

--- www/src/Controller/MessageController.php
+++ www/src/Controller/MessageController.php
@@ -2,39 +2,27 @@
 
 namespace App\Controller;
 
-<<<<<<< HEAD
 use App\Service\StompMessageSender;
 use Stomp\Exception\StompException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-=======
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
->>>>>>> 2e401ef5e1a5e300eb11470a77d9c44edbc87be4
 use Symfony\Component\Routing\Annotation\Route;
-use Stomp\Client;
-use Stomp\SimpleStomp;
-use Stomp\Transport\Bytes;
 
 class MessageController extends AbstractController
 {
-<<<<<<< HEAD
     private StompMessageSender $sender;
 
     public function __construct(StompMessageSender $sender)
     {
         $this->sender = $sender;
     }
-=======
->>>>>>> 2e401ef5e1a5e300eb11470a77d9c44edbc87be4
 
     /**
      * @Route("/message/test", methods={"GET"})
      */
     public function test()
     {
-<<<<<<< HEAD
         try {
             $messageId = $this->sender->send(
                 '/queue/test',
@@ -44,7 +32,7 @@ class MessageController extends AbstractController
         } catch (StompException $e) {
             return new JsonResponse($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-=======
+
         $client = new Client('tcp://artemis:61613');
         $client->setLogin("admin", "admin");
         $stomp = new SimpleStomp($client);
@@ -54,7 +42,6 @@ class MessageController extends AbstractController
         $stomp->send('/queue/test', $bytesMessage);
 
         return new JsonResponse(['message sent' => $messageId]);
->>>>>>> 2e401ef5e1a5e300eb11470a77d9c44edbc87be4
     }
 
 }
